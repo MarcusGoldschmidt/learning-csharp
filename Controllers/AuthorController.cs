98ce14856a4113a2ad75ba93d45e751ae29e8048
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-05-15 16:01:21 -0400

    CRUD Author

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -1,6 +1,9 @@
-using System.Collections.Generic;
+using System.Collections;
+using System.Threading.Tasks;
+using FistApi.Database.Migrations;
 using FistApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FistApi.Controllers
 {
@@ -8,11 +11,51 @@ namespace FistApi.Controllers
     [ApiController]
     public class AuthorController
     {
-        /*// GET api/values
+        // GET api/values
         [HttpGet]
-        public ActionResult<List<Author>> Get()
+        public async Task<IList> Get()
         {
+            return await new MainContext().Authors.ToListAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<Author> Get(int id)
+        {
+            MainContext db = new MainContext();
+            return await db.Authors.FindAsync(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<ActionResult<Author>> Post([FromForm]Author value)
+        {
+            MainContext db = new MainContext();
+            var response = db.Authors.Add(value).Entity;
+            await db.SaveChangesAsync();
+            return new ObjectResult(response);
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public async Task<Author> Put([FromForm] Author value)
+        {
+            MainContext db = new MainContext();
+            db.Authors.Update(value);
+            await db.SaveChangesAsync();
+            return value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public async Task<Author> Delete([FromForm] int id)
+        {
+            MainContext db = new MainContext();
+            var respose = await db.Authors.FindAsync(id);
+            db.Authors.Remove(respose);
+            await db.SaveChangesAsync();
             
-        }*/
+            return respose; 
+        }
     }
 }
